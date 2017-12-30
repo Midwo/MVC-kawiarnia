1b797d5bd3d01eb7ae993fb05a5d9fdf71f6667f
Midwo <michal.dwojak92@gmail.com>
2017-12-30 22:51:09 +0100

    Add project files.

---
+++ MVC kawiarnia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_kawiarnia.Startup))]
+namespace MVC_kawiarnia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
